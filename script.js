47ef2f0d30624d6fa9fa5fb6cb93aea4820a88de
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-15 14:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const submitButton=document.getElementById('submit')
+const formContainer=document.querySelector('form');
+
+submitButton.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const username=document.getElementById('username').value;
+    const password=document.getElementById('password').value;
+    const rememberMe=document.getElementById('checkbox');
+
+    const localUsername=window.localStorage.getItem('username');
+    console.log(localUsername);
+    if(localUsername===null){
+        if(rememberMe.checked){
+            window.localStorage.setItem('username',JSON.stringify(username));
+            window.localStorage.setItem('password',JSON.stringify(password));
+        }
+        else{
+            window.localStorage.removeItem('username');
+            window.localStorage.removeItem('password');
+        }
+        alert("Logged in as.")
+    }   
+    else if(localUsername!==null){
+        const existingButton = document.createElement('button');
+        existingButton.textContent = 'Login as existing user';
+        existingButton.type = 'button'; 
+        formContainer.appendChild(existingButton);
+
+        existingButton.addEventListener('click', () => {
+            alert(`Logged in as ${localUsername}`);
+        });
+    }
+})
